a003ca4969f8672f99d846105bbaac2f5902209d
muhammad-rawjee <muhammadrawjee@gmail.com>
2023-02-19 15:37:25 -0500

    Edit ContainsDuplicate.py

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -1,6 +1,19 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        '''
         for i in range(len(nums)):
             if nums[i] in nums[i + 1:]:
                 return True
+        return False
+        '''
+
+        hashset = set()
+
+        for num in nums:
+            if num in hashset:
+                return True
+            
+            hashset.add(num)
+
         return False
