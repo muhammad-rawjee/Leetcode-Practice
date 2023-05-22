fb8731d0b1b7217a9713a7c1e19f4a2b5fa3f14f
muhammad-rawjee <muhammadrawjee@gmail.com>
2023-05-21 21:12:27 -0400

    Redo

--- Arrays-and-Hashing/EncodeDecode.py
+++ Arrays-and-Hashing/EncodeDecode.py
@@ -6,9 +6,8 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         result = ""
-
         for s in strs:
-            result += str(len(s)) + "$" + s
+            result += str(len(s)) + "#" + s
         
         return result
 
@@ -16,18 +15,22 @@ class Codec:
         """Decodes a single string to a list of strings.
         """
         result, i = [], 0
+
         while i < len(s):
             j = i
-            while s[j] != "$":
+            while s[j] != '#':
                 j += 1
+            
             length = int(s[i:j])
             result.append(s[j + 1: j + 1 + length])
+            
             i = j + 1 + length
-        return result
+
+        return result        
 
         
 
-strs = "Why wont recruiters respond".split()
+strs = "Hello World, How are you?".split()
 #Your Codec object will be instantiated and called as such:
 codec = Codec()
 print(codec.encode(strs))
